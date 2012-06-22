8045d0ca0873d86fe502992915cc461446a9c260
Alok K Shukla <kalokshukla@gmail.com>
2012-06-22 14:19:01 +0530

    returning a set of all words who are just one letter different.

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -13,54 +13,54 @@
 #include "vector.h"
 using namespace std;
 
-//Set< Set < string > > nextWords(Lexicon & english,string word);
+Set<string> nextWords(Lexicon & english,string word);
 Set<string> nextWordsSpec(Lexicon & english,string word, int pos);
 
 
 int main() {
 	Lexicon english("EnglishWords.dat");
-    cout<<english.size();
     string word="code";
-    Set<string> next=nextWordsSpec(english,word,1);
-    cout<<endl;
-        int i=0;
+    Set<string> set;
     cout<<"[ ";
-    foreach(string temp in next){
-        i++;
-        if (i!=1) {
-            cout<<", "<<temp;
+    int i=0;
+    set=nextWords(english,word);
+    foreach(string s in set){
+        if (i++!=0) {
+            cout<<", "<<s;
         }
         else {
-            cout<<temp;
+            cout<<s;
+
         }
     }
     cout<<" ]"<<endl;
-    cout<<endl;
 	return 0;
 }
 
-/*Set< Set < string > > nextWords(Lexicon & english,string word){
-    Set<Set < string > > allNext;
-    for (int i=0; i<word.length(); i++) {
-        allNext.add(nextWordsSpec(english,word, i));
-        
-    }
-    return allNext;
-}
-*/
+ Set<string> nextWords(Lexicon & english,string word){
+ Set<string> allNext;
+ for (int i=0; i<word.length(); i++) {
+     foreach (string s in nextWordsSpec(english, word, i)){
+         allNext.add(s);
+     }
+ 
+ }
+ return allNext;
+ }
+ 
 
 Set<string> nextWordsSpec(Lexicon & english,string word, int pos){
     string temp=word;
     Set<string> next;
     char c=word[pos];
     for (int i=0; i<26; i++) {
-    if (char(i+'a')!=c){
-            word[1]=char(i+'a');
+        if (char(i+'a')!=c){
+            word[pos]=char(i+'a');
             if (english.contains(word)) {
-                        next.add(word);
+                next.add(word);
             }
-    }
- 
+        }
+        
     }
     return next;
 }
