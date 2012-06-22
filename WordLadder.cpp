62f39384a33a95868571e56cc852c2402671750c
Alok K Shukla <kalokshukla@gmail.com>
2012-06-22 13:06:46 +0530

    This thing should probably work, but it isn't. When working it produces a list of all words who have only one character different from "code" but are still meaningful.

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -13,23 +13,44 @@
 #include "vector.h"
 using namespace std;
 
-Set<string> nextWords(string word);
-Set<string> nextWordsSpec(string word, int pos);
+Set< Set < string > > nextWords(Lexicon & english,string word);
+Set<string> nextWordsSpec(Lexicon & english,string word, int pos);
 
 
 int main() {
 	Lexicon english("EnglishWords.dat");
     cout<<english.size();
     string word="code";
-    Set<string> next;
-    next=nextWords(word);
-    foreach(string temp in next){
-        cout<<temp<<" ";
+    Set< Set < string > > next;
+    next=nextWords(english,word);
+    foreach(Set<string> temp in next){
+        foreach(string temp2 in temp){
+            cout<<temp2<<" ";
+        }
     }
     cout<<endl;
 	return 0;
 }
 
-Set<string> nextWords(string word){
-    
+Set< Set < string > > nextWords(Lexicon & english,string word){
+    Set<Set < string > > allNext;
+    for (int i=0; i<word.length(); i++) {
+        allNext.add(nextWordsSpec(english,word, i));
+        
+    }
+    return allNext;
+}
+
+Set<string> nextWordsSpec(Lexicon & english,string word, int pos){
+    string temp=word;
+    Set<string> set;
+    int i;
+    for (i=0; i<26; i++) {
+        temp[pos]=char(i+'a');
+        if (english.contains(temp)) {
+            set.add(temp);
+
+        }
+    }
+    return set;
 }
